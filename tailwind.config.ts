1a37052b83cc17c203216980f33a6d3654bfb2a3
hyeonseok98 <157561573+hyeonseok98@users.noreply.github.com>
2025-11-21 16:39:40 +0900

    ✨feat: 멀티뷰 데모

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,13 +9,13 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        chzzkBackground: "#050814",
-        chzzkPanel: "#0B1020",
-        chzzkBorder: "#1B2236",
-        chzzkAccent: "#00FFA3",
-        chzzkAccentSoft: "#00C97D",
-        chzzkTextPrimary: "#F9FAFB",
-        chzzkTextSecondary: "#9CA3AF",
+        chzzBackground: "#050814",
+        chzzPanel: "#0B1020",
+        chzzBorder: "#1B2236",
+        chzzAccent: "#00FFA3",
+        chzzAccentSoft: "#00C97D",
+        chzzTextPrimary: "#F9FAFB",
+        chzzTextSecondary: "#9CA3AF",
       },
     },
   },
