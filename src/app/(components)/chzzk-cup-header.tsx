2bd79d919c3dde75da9726a0edecdeaac0c8e460
Jamong <arozic425@gmail.com>
2025-11-26 20:11:38 +0900

    🚑 hotfix: chzz -> chzzk 오타 수정 (#15)

--- src/app/(components)/chzzk-cup-header.tsx
+++ src/app/(components)/chzzk-cup-header.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 import Link from "next/link";
 
 const NAV_ITEMS = [
-  { label: "경기 일정", href: "/chzz-cup-schedule" },
+  { label: "경기 일정", href: "/chzzk-cup-schedule" },
   // { label: "팀 & 선수", href: "#teams" },
   { label: "모의 드래프트", href: "/chzzk-draft" },
   // { label: "클립 다시보기", href: "#clips" },
